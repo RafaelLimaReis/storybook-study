735d1026c91c1f4a8bc1795edcde24857050f30b
Rafael Reis <rafaelreis@gurgelmix.com.br>
2025-02-13 10:48:51 -0300

    dando nomes para os components dentro do storybooks e melhorando a doc

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/RafaelLimaReis.png',
     alt: 'Rafael Lima'
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text'
+      }
+    }
+  }
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -5,8 +5,31 @@ export default {
   title: 'Typography/Text',
   component: Text,
   args: {
+    size: 'md',
     children: 'Cillum excepteur nulla velit proident duis. Ipsum incididunt duis sit nulla. Mollit voluptate fugiat est culpa.',
   },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio'
+      }
+    }
+  }
 } as Meta<TextProps>
 
 export const Primary: StoryObj<TextProps> = {}
